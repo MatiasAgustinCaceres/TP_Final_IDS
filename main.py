ec0e05e0483f4ffa6be96406f2812b201b71e771
Matias Caceres <matiascaceres.3197@gmail.com>
2024-07-27 02:59:07 -0300

    back listo para empezar a construir el front

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
 from flask import Flask
-from models import db, Author, Book
-
+from models import db, Vala, Troop, Campaign
 app = Flask(__name__)
 port = 5000
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:123450@localhost:5432/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
-@app.route('/')
-def hello_world():
-    return 'Hello world! CARAJO!!!!!!!'
+#@app.route()
+#def hello_world():
+#    return 'Hello world! CARAJO!!!!!!!'
+
+@app.route('/', methods=['GET'])
+def get_Valar():
+    try:
+        #Trigo la info de los Valar
+        valar = Vala.query.all()
+        valarData = []
+        for vala in valar:
+            valaData = {
+                'id': vala.id,
+                'name': vala.description,
+            }
+            valarData.append(valaData)
+        
+        #Traigo la info de las Tropas
+        Troops = Troop.query.all()
+        TroopsData = []
+        for troop in Troops:
+            TroopData = {
+                'id': troop.id,
+                'name': troop.name,
+                'description': troop.description,
+                'combatPower': troop.combatPower,
+                'consumption': troop.consumption,
+            }
+            TroopsData.append(TroopData)
+        
+        #Muestro la info por consola
+        print(valarData)     
+        print(TroopsData)   
+        return 'COOL'
+    except Exception as error:
+        print('Error', error)
+        return 'Error', 500
 
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        db.create_all()  
     app.run(host='0.0.0.0', debug=True, port=port)
--- models.py
+++ models.py
@@ -1,21 +1,29 @@
-import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Author(db.Model):
-    __tablename__ = 'authors'
+class Vala(db.Model):
+    __tablename__ = 'Valar'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
-    books = db.relationship("Book")
+    description = db.Column(db.String(255), nullable=False)
 
-class Book(db.Model):
-    __tablename__ = 'books'
+class Troop(db.Model):
+    __tablename__ = 'Troops'
     id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
-    cant_pages = db.Column(db.Integer, nullable=False, default=0)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    description = db.Column(db.String(255), nullable=False)
+    combatPower = db.Column(db.Integer, nullable=False)
+    consumption = db.Column(db.Integer, nullable=False)
+
+class Campaign(db.Model):
+    __tablename__ = 'Campaigns'
+    id = db.Column(db.Integer, primary_key=True)
+    vala = db.Column(db.Integer, db.ForeignKey('Valar.id'))
+    generalName = db.Column(db.String(255), nullable=False)
+    troopOne = db.Column(db.Integer, db.ForeignKey('Troops.id'))
+    troopTwo = db.Column(db.Integer, db.ForeignKey('Troops.id'))
+    troopThree = db.Column(db.Integer, db.ForeignKey('Troops.id'))
+    troopFour = db.Column(db.Integer, db.ForeignKey('Troops.id'))
+    status = db.Column(db.String(255), nullable=False)
+    totalCombatPower = db.Column(db.Integer, nullable=False)
